e466d2c987eeaeada17771ed8538b205159a6be4
Rafael Dametto <cacotpds@gmail.com>
2023-12-28 14:50:35 -0300

    Styling nav-bar and title

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { BlogNewsComponent } from '../blog-news/blog-news.component';
+import { BlogLibraryComponent } from '../blog-library/blog-library.component';
+import { YoutubeVideosComponent } from '../youtube-videos/youtube-videos.component';
+import { ContactUsComponent } from '../contact-us/contact-us.component';
+import { FooterComponent } from '../footer/footer.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NavBarComponent,
+    NavBarComponent,
+    BlogNewsComponent,
+    BlogLibraryComponent,
+    YoutubeVideosComponent,
+    ContactUsComponent,
+    FooterComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
